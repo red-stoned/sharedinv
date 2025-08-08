983146c936475bc7f46011fbf7ab1d97f27b36c9
galiano <piz@redstoned.dev>
2025-08-08 17:19:40 -0500

    changed injects

--- src/main/java/com/redstoned/sharedinv/mixin/PlayerInventoryMixin.java
+++ src/main/java/com/redstoned/sharedinv/mixin/PlayerInventoryMixin.java
@@ -3,11 +3,14 @@ package com.redstoned.sharedinv.mixin;
 import com.redstoned.sharedinv.*;
 import net.minecraft.entity.EntityEquipment;
 import net.minecraft.entity.player.PlayerEquipment;
+import net.minecraft.inventory.StackWithSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.storage.WriteView;
 import net.minecraft.util.collection.DefaultedList;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.entity.player.PlayerEntity;
@@ -20,16 +23,16 @@ public class PlayerInventoryMixin implements IPlayerInventory {
 	@Mutable @Final @Shadow private DefaultedList<ItemStack> main;
 	@Mutable @Shadow @Final private EntityEquipment equipment;
 
-	@Inject(method = "writeNbt", at = @At("HEAD"))
-	private void inplaceOriginalInventoryOnWrite(NbtList nbtList, CallbackInfoReturnable<NbtList> ci) {
+	@Inject(method = "writeData", at = @At("HEAD"))
+	private void inplaceOriginalInventoryOnWrite(WriteView.ListAppender<StackWithSlot> list, CallbackInfo ci) {
 		if (SharedInventory.playerInvs.containsKey(player.getUuid())) {
 			// SharedInventoryMod.LOGGER.info("[DEBUG] Player is in team at begin write time, resetting their inv to point to the original");
 			SharedInventoryMod.RestorePlayerSlots(player);
 		}
 	}
 
-	@Inject(method = "writeNbt", at = @At("TAIL"))
-	private void rejoinTeamAfterWriteInventory(NbtList nbtList, CallbackInfoReturnable<NbtList> ci) {
+	@Inject(method = "writeData", at = @At("TAIL"))
+	private void rejoinTeamAfterWriteInventory(WriteView.ListAppender<StackWithSlot> list, CallbackInfo ci) {
 		SharedInventory inv = SharedInventory.playerInvs.get(player.getUuid());
 		if (inv != null) {
 			// SharedInventoryMod.LOGGER.info("[DEBUG] Player is in team at end write time, resetting their inv to point to the shared");
